b90e56dbe77571369e56dc70d9c3113c8da6fdb1
Matheus Silva <matheuss.cirq@outlook.com>
2023-12-22 13:04:49 -0300

    Update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-function alterarStatus(id){
-    let click = document.getElementById(`game-$(id)`);
+function alterarStatus(id) {
+    let click = document.getElementById(`game-${id}`);
     let blur = click.querySelector(`.dashboard__item__img`);
     let bot = click.querySelector(`.dashboard__item__button`);
     let nome = click.querySelector(`.dashboard__item__name`);
     
-    
+    alert("Você reservou " + nome.textContent);
 }
