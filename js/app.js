b00b3616288e57b14d3e2d9eba1c958dcd52750a
Matheus Silva <matheuss.cirq@outlook.com>
2023-12-22 15:25:48 -0300

    Adding info for console

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+let rented = 0;
+
+function updateRentered(){
+    rented = document.querySelectorAll('.dashboard__item__img--rented').length;
+    console.log(`À quantiadade total de jogos alugados é igual a ${rentedCount}`);
+} 
+
+
 function alterarStatus(id) {
     let click = document.getElementById(`game-${id}`);
     let blur = click.querySelector(`.dashboard__item__img`);
@@ -9,12 +17,14 @@ function alterarStatus(id) {
         blur.classList.remove('dashboard__item__img--rented');
         bot.textContent = 'Alugar';
         bot.classList.remove('dashboard__item__button--return');
-        alert("Você reservou " + nome.textContent);
+        alert("Você devolveu " + nome.textContent);
     } else {
         blur.classList.add('dashboard__item__img--rented');
         bot.textContent = 'Devolver';
         bot.classList.add('dashboard__item__button--return');
-        alert("Você devolveu " + nome.textContent);
+        alert("Você reservou " + nome.textContent);
     }
+    updateRentered();
 }
+updateRentered();
 
