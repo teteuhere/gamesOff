717759f59ac390b1737c62dbb1d1a53a2cd79a8a
Matheus Silva <matheuss.cirq@outlook.com>
2023-12-22 12:43:19 -0300

    Updating app.js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function alterarStatus(id){
+    let click = document.getElementById(`game-$(id)`);
+    let blur = click.querySelector(`.dashboard__item__img`);
+    let bot = click.querySelector(`.dashboard__item__button`);
+    let nome = click.querySelector(`.dashboard__item__name`);
+    
+    
+}
